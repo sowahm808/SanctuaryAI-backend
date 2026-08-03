278412b10a0cae367199fb6b308c7e2efa8cb09d
Michael Sowah <sowahm@gmail.com>
2026-08-03 18:54:32 -0500

    updated

--- src/main.ts
+++ src/main.ts
@@ -3,23 +3,36 @@ import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import { ConfigService } from "@nestjs/config";
 import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
-import helmet from "helmet";
-import compression from "compression";
-import cookieParser from "cookie-parser";
+
+import helmet = require("helmet");
+import compression = require("compression");
+import cookieParser = require("cookie-parser");
+
 import { AppModule } from "./app.module";
 import { EnvelopeInterceptor } from "./common/envelope.interceptor";
 import { ProblemFilter } from "./common/problem.filter";
-async function bootstrap() {
+
+async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule, { bufferLogs: true });
   const config = app.get(ConfigService);
+
   app.setGlobalPrefix("api/v1");
+
   app.use(helmet());
   app.use(compression());
   app.use(cookieParser());
+
+  const corsOrigins = config
+    .getOrThrow<string>("CORS_ORIGINS")
+    .split(",")
+    .map((origin) => origin.trim())
+    .filter(Boolean);
+
   app.enableCors({
-    origin: config.getOrThrow<string>("CORS_ORIGINS").split(","),
+    origin: corsOrigins,
     credentials: true,
   });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -28,22 +41,26 @@ async function bootstrap() {
       errorHttpStatusCode: 422,
     }),
   );
+
   app.useGlobalInterceptors(new EnvelopeInterceptor());
   app.useGlobalFilters(new ProblemFilter());
-  if (config.get<boolean>("SWAGGER_ENABLED"))
-    SwaggerModule.setup(
-      "docs",
-      app,
-      SwaggerModule.createDocument(
-        app,
-        new DocumentBuilder()
-          .setTitle("SanctuaryAI API")
-          .setVersion("1.0")
-          .addBearerAuth()
-          .build(),
-      ),
-    );
+
+  if (config.get<boolean>("SWAGGER_ENABLED")) {
+    const swaggerConfig = new DocumentBuilder()
+      .setTitle("SanctuaryAI API")
+      .setVersion("1.0")
+      .addBearerAuth()
+      .build();
+
+    const document = SwaggerModule.createDocument(app, swaggerConfig);
+
+    SwaggerModule.setup("docs", app, document);
+  }
+
   app.enableShutdownHooks();
-  await app.listen(config.get<number>("PORT") ?? 3000, "0.0.0.0");
+
+  const port = config.get<number>("PORT") ?? 3000;
+  await app.listen(port, "0.0.0.0");
 }
+
 void bootstrap();
